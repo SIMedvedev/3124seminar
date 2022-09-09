145db09313560b083880f02007851b1a35054189
Sergey Medvedev <med___ved@mail.ru>
2022-09-09 06:30:55 +0300

    +task11

---
+++ seminar2/task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.Write("Сгенерировалось число " + number  +" -> абра кадабра -> ");
+int digit1 = number / 100;
+int digit2 = number % 10;
+Console.Write(digit1); 
+Console.WriteLine(digit2); 
--- seminar2/task9/Program.cs
+++ seminar2/task9/Program.cs
@@ -4,15 +4,21 @@
 
 Random rand = new Random();
 int number = rand.Next(10, 100);
-Console.WriteLine(number);
+Console.WriteLine($"Сгенерировалось случайное число {number}");
 int digit2 = number % 10;
 int digit1 = number / 10;
-
+// Console.WriteLIne(digit1);
+// Console.WriteLIne(digit2);
+ 
 if (digit1 > digit2)
 {
-    Console.WriteLine("Первая цифра больше второй");
+    Console.WriteLine($"Цифра {digit1} больше цифры {digit2}");
+}
+else if (digit1 < digit2)
+{
+    Console.WriteLine($"Цифра {digit2} больше цифры {digit1}");
 }
 else 
 {
-        Console.WriteLine("Вторая цифра больше первой");
+    Console.WriteLine("Цифры равны");
 }
