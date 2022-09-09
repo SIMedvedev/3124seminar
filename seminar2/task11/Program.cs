b66d05216e1907dd48475a38e02f75c9cbe8404b
Sergey Medvedev <med___ved@mail.ru>
2022-09-09 10:18:49 +0300

    +task12

--- seminar2/task11/Program.cs
+++ seminar2/task11/Program.cs
@@ -7,5 +7,8 @@ int number = rand.Next(100, 1000);
 Console.Write("Сгенерировалось число " + number  +" -> абра кадабра -> ");
 int digit1 = number / 100;
 int digit2 = number % 10;
+// int res = digit1 * 10 + digit2 умножаем на 10, так как первая цифра это десятки в 
+//двухзнвчном чсисле, т.е. 5*10 + 5 == 55
+// Соответственно вывод Console.WriteLine(res);
 Console.Write(digit1); 
 Console.WriteLine(digit2); 
---
+++ seminar2/task12/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли 
+// первое число кратно второму. Если число 1 не кратно чсислу 2, то программа выводит 
+// остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите число 1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+if (N2 !=0)
+{
+    int mod = N1 % N2;
+    if (mod ==0)
+    {
+        Console.WriteLine("кратно");
+    }
+    else
+    {
+        Console.WriteLine($"не кратно, остаток {mod}");
+    }
+}
+else
+{
+    Console.WriteLine("нельзя делить на ноль");
+}
+
+// if (N1 % N2 == 0)
+// {
+//     Console.WriteLine("кратно");
+// }
+// else 
+// {
+//     Console.WriteLine($"не кратно, остаток {N1 % N2}");
+// }
