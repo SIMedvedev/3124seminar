d1ee7386fc13cfa93f4b1b08e4ae3bc41ce88b13
Sergey Medvedev <med___ved@mail.ru>
2022-09-12 21:04:08 +0300

    +task20+22

---
+++ seminar3/task20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Write("Введите координату первой точки по X ");
+double xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату первой точки по Y ");
+double yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату второй точки по X ");
+double xB= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату второй точки по Y ");
+double yB = Convert.ToInt32(Console.ReadLine());
+// double x = Math.Pow(xB - xA, 2); // возвести в степень
+// double y = Math.Pow(yB - yA , 2);
+// double distance = Math.Sqrt(x + y);  // найти квадратный корень
+double distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+Console.WriteLine($"{distance:f4}"); // осатвим 4 числа после запятой
+                                     // f - указывает, что число вещественное
---
+++ seminar3/task22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (number > 0)
+{
+    while (i <= number)
+    {
+        Console.Write(i*i + " ");
+        i++;
+    }
+}
+else 
+{
+    Console.Write("Число введено не верно");
+}
