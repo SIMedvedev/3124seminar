0e33ec1bca1dab27b032b98489fcc194957bf8ad
Sergey Medvedev <med___ved@mail.ru>
2022-09-16 08:27:43 +0300

    +list

---
+++ seminar4/task24/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (А) и 
+// выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(result);
+
+
