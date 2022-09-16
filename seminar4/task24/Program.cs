3f569663df84e113ca3a9b84df2ddb81053a9179
Sergey Medvedev <med___ved@mail.ru>
2022-09-16 09:14:32 +0300

    +task24

--- seminar4/task24/Program.cs
+++ seminar4/task24/Program.cs
@@ -3,11 +3,44 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+// напишем метод, который будет возвращать целое число
+// Console.WriteLine("Введите число1:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(number1);
+// Console.WriteLine("Введите число2:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(number2);
 
-Console.WriteLine("Введите число:");
-int number = Convert.ToInt32(Console.ReadLine());
+int ReadNumber(string message) // напишем метод, который будет возвращать целое число
+{   
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNymbersFrom1ToA(int a) //метод для нахождения суммы чисел от 1 до А
+{
+    if (a >= 1)  
+    {   
+        int sum = 0;
+        for (int i = 1; i <=a; i++)
+        {
+            sum = sum + i; // sum +=i;
+        }
+        return sum; //вернем накопившуюся сумму
+    }
+    else
+    {
+        Console.WriteLine("А должна быть не меньше 1");
+        return -1; //неправильная сумма(код ошибки) 
+    }
+}
+int number = ReadNumber("Введите число A:");
+int summa = SumNymbersFrom1ToA(number);
+Console.Write($"Сумма элементов от 1 до А = {summa}");
 
 
-Console.WriteLine(result);
+
+
+
 
 
