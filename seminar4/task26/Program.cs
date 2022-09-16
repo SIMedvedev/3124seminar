c53c1854edb7348a614c3b2bf6e944bad7f88121
Sergey Medvedev <med___ved@mail.ru>
2022-09-16 11:10:38 +0300

    +task26

---
+++ seminar4/task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int readNumber (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int numberOfDigits(int n)
+{
+    int i = 0;
+    for (i = 0; n > 0; i++)
+    {
+        n = n / 10;
+    }
+    return i;
+}
+int number = readNumber ("Введите число");
+int digits = numberOfDigits(number);
+Console.WriteLine($"Количетсво цифр = {digits}");
