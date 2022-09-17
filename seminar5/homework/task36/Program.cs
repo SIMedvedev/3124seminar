bb788b92f8864d8c23b8304bcce7be7b725534cb
Sergey Medvedev <med___ved@mail.ru>
2022-09-17 20:01:10 +0300

    +task36_hw

---
+++ seminar5/homework/task36/Program.cs
@@ -0,0 +1,23 @@
+﻿// : Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+//  стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int[] array = GetArray(10, 0, 5);
+Console.WriteLine(string.Join(",", array));
+int count = 0;
+for (int i = 1; i < array.Length; i +=2)
+{
+    count = count + array[i]; 
+}
+Console.WriteLine("Сумма элементов чисел равна " + count);
---
+++ seminar5/homework/task38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив натуральных чисел. Найдите разницу между максимальным и 
+// минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int[] array = GetArray(10, 0, 5);
+Console.WriteLine(string.Join(",", array));
