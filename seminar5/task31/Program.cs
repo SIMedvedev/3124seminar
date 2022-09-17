b315048843abd63f123b0bc00af2dfcd905be55e
Sergey Medvedev <med___ved@mail.ru>
2022-09-17 15:11:48 +0300

    +task32

--- seminar5/task31/Program.cs
+++ seminar5/task31/Program.cs
@@ -5,42 +5,64 @@
 
 int[] GetArray(int size, int leftRange, int rightRange) //границы случайного диапазона left right
 {
-    int[] arr = new int[size];
-    Random rand = new Random(); // создаем переменную рандома
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rand.Next(leftRange, rightRange + 1);
-    }
-    return arr;
-}
-int SumPositiveElements(int[] arr)
+     int[] arr = new int[size];
+     Random rand = new Random(); // создаем переменную рандома
+     for (int i = 0; i < size; i++)
+     {
+         arr[i] = rand.Next(leftRange, rightRange + 1);
+     }
+     return arr;
+ }
+// int SumPositiveElements(int[] arr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > 0) // если элемент положительный 
+//         {
+//             sum = sum + arr[i];  //  копим сумму положительных элементов sum += arr[i]
+//         }
+//     }
+//     return sum;
+// }
+// int SumNegativeElements(int[] arr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] < 0) // если элемент отрицательный 
+//         {
+//             sum = sum + arr[i];  //  копим сумму орицательных элементов sum += arr[i]
+//         }
+//     }
+//     return sum;
+// }
+// int[] array = GetArray(12, -9, 9);
+// Console.WriteLine(string.Join(",", array));
+// int sumPositive = SumPositiveElements(array);
+// int sumNegative = SumNegativeElements(array);
+// Console.WriteLine($"Сумма положительных элементов = {sumPositive}, отрицательных элементов = {sumNegative}");
+
+
+(int, int) GetPositiveAndNegativ(int[] arr) // метод с возвращением кортежа
 {
-    int sum = 0;
+    int sumP = 0;
+    int sumN = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] > 0) // если элемент положительный 
+        if (arr[i] > 0)
         {
-            sum = sum + arr[i];  //  копим сумму положительных элементов sum += arr[i]
+            sumP += arr[i];
+        }
+        else
+        {
+            sumN += arr[i];
         }
     }
-    return sum;
-}
-int SumNegativeElements(int[] arr)
-{
-    int sum = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] < 0) // если элемент отрицательный 
-        {
-            sum = sum + arr[i];  //  копим сумму орицательных элементов sum += arr[i]
-        }
-    }
-    return sum;
+    return (sumP, sumN);
 }
 int[] array = GetArray(12, -9, 9);
 Console.WriteLine(string.Join(",", array));
-int sumPositive = SumPositiveElements(array);
-int sumNegative = SumNegativeElements(array);
+(int sumPositive, int sumNegative) = GetPositiveAndNegativ(array);
+
 Console.WriteLine($"Сумма положительных элементов = {sumPositive}, отрицательных элементов = {sumNegative}");
-
-
---
+++ seminar5/task32/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу замена элементов массива: положительные элементы 
+// замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GetArray(int size, int leftRange, int rightRange) //границы случайного диапазона left right
+{
+     int[] arr = new int[size];
+     Random rand = new Random(); // создаем переменную рандома
+     for (int i = 0; i < size; i++)
+     {
+         arr[i] = rand.Next(leftRange, rightRange + 1);
+     }
+     return arr;
+ }
+ int[] newArr(int[] arr)  // возвращаем массив
+ {
+     
+     for (int i = 0; i < arr.Length; i++)
+             { 
+                arr[i] = arr[i] * -1;
+            }
+             return arr; 
+}
+ int[] array = GetArray(12, -9, 9);
+
+ Console.WriteLine(string.Join(",", array));
+int[] newarr = newArr(array);
+
+Console.WriteLine(string.Join(",", newarr));
