a06398ad4d4be379e950959dc5ba43441deb5b39
Sergey Medvedev <med___ved@mail.ru>
2022-09-27 19:53:47 +0300

    +seminar7

---
+++ seminar7/task46/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int ReadNumber(string message) // Ввод числа из консоли.
+{
+  Console.WriteLine(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rows, int columns, int leftRange = -10, int rightRange = 10) // Наполнение массива случайными числами.
+{
+  int[,] matrix = new int[rows, columns];
+  Random rand = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // Вывод массива в терминал.
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+int m = ReadNumber("Введите количество строк матрицы: ");
+int n = ReadNumber("Введите количество столбцов матрицы: ");
+
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
