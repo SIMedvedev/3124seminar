002a8a847241e7b1963446c74fc88602ac6203ef
Sergey Medvedev <med___ved@mail.ru>
2022-10-02 21:27:04 +0300

    +lesson7

---
+++ lesson7/CatalogInfo/Program.cs
@@ -0,0 +1,45 @@
+﻿// Как посмотреть содержимое папки?
+
+// string path = "C:/Users/Сергей/Desktop/ОБУЧЕНИЕ ГБ/дз2";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+void CatalogInfo(string path, string indent = "")  // string indent = "" делает отспупы, чтобы лучшк видеть структуры папки
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i  < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i  < files.Length; i++)
+    {
+         Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = @"C:\Users\Сергей\Desktop\ОБУЧЕНИЕ ГБ";
+CatalogInfo(path);
+
+// void CatalogInfo(string path, string indent = "")
+// {
+//     DirectoryInfo catalogs = new DirectoryInfo(path);
+//     foreach (var currentCatalog in catalogs.GetDirectories())
+//     {
+//         Console.WriteLine($"{indent}{currentCatalog.Name}");
+//         CatalogInfo(currentCatalog.FullName, indent + " ");
+//     }
+//     foreach (var item in catalogs.GetFiles())
+//     {
+//         Console.WriteLine($"{indent}{item.Name}");
+//     }
+// }
+// string path = @"C:\Users\Сергей\Desktop\ОБУЧЕНИЕ ГБ\Новая папка";
+// CatalogInfo(path);
---
+++ lesson7/fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿// Фибоначчи
+
+decimal fRec =0;
+decimal fIte =0;
+//рекурсивный подход
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n==0 || n ==1 ? 1 : FibonacciRecursion(n-1) + FibonacciRecursion(n-2);
+}
+
+// метод подсчета иттераций
+decimal FibonacciIteracion(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1= 2;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+        fIte++;
+    }
+   return result;
+}
+
+
+for (int n = 1; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteracion({n}) = {FibonacciIteracion(n)} fIte = {fIte.ToString("### ### ###"), -15}");
+    fIte = 0;
+}
+Console.WriteLine();
+
+for (int n = 1; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fIte = {fRec.ToString("### ### ###"), -15}");
+    fRec = 0;
+}
